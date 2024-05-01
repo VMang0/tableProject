f84149f1395008248367d601fec4e25966bc31d9
VMang0 <valeriakorolkova2703@gmail.com>
2024-05-01 14:57:07 +0300

    refactor: redux update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import report from '@redux/slices/report-slice';
+import reportInfo from '@redux/slices/report-info-slice';
 
 const rootReducer = combineReducers({
-    router: routerReducer
+    report,
+    reportInfo
 });
 
 export const store = configureStore({
     reducer: rootReducer
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
