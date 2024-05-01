9642dd3869621fd591041ff735e1dbc0fe44607a
VMang0 <valeriakorolkova2703@gmail.com>
2024-05-02 02:40:07 +0300

    refactor: change main.tsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { App } from '@components/app';
 import '@utils/style/default.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
